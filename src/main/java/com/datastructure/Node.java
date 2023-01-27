c67f14be8507d1a5e471e1a674024d25319d2d24
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-26 20:02:38 -0800

    Added: program to Create a Simple Linked List

---
+++ src/main/java/com/datastructure/Node.java
@@ -0,0 +1,12 @@
+package com.datastructure;
+
+public class Node {
+    
+    int key;
+    Node next;
+        public Node(int key)
+        {
+            this.key = key;
+            this.next=null;
+        }
+    }
