9325fcd0958a1b630027cbb6d00b8d0499763fb5
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-26 20:53:14 -0800

    Added: Ability to Insert Between the final Sequence

--- src/main/java/com/datastructure/Main.java
+++ src/main/java/com/datastructure/Main.java
@@ -18,6 +18,8 @@
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Enter 1 to add the data at the start"); // UC 2
                 System.out.println("Enter 2 to add the data at the End"); // UC 3
+                System.out.println("Enter 3 to add the data at In Between "); // UC 4
+        
                 switch (scanner.nextInt()) {
         
                     case 1:
@@ -26,8 +28,9 @@
                     case 2:
                         Operation.addDataAtEnd();
                         break;
-        
+                    case 3:
+                        Operation.insertInBetween();
+                        break;
                 }
             }
         }
-        
--- src/main/java/com/datastructure/Operation.java
+++ src/main/java/com/datastructure/Operation.java
@@ -2,8 +2,8 @@ package com.datastructure;
 
 public class Operation {
     
-      //created method to add data at start.
-      public static void addDataAtStart(){
+     //created method to add data at start.
+     public static void addDataAtStart(){
         LinkedList linkedList =new LinkedList();
         linkedList.push(70);
         linkedList.push(30);
@@ -17,6 +17,20 @@ public class Operation {
         linkedList.append(56);
         linkedList.append(30);
         linkedList.append(70);
+        linkedList.print();
+
+    }
+    //created method for insert in between
+    public static void insertInBetween(){
+        LinkedList linkedList=new LinkedList();
+        Node secondNode =linkedList.push(70);
+        Node firstNode =linkedList.push(56);
+        Node newNode =new Node(30);
+        System.out.println("Before");
+        linkedList.print();
+        linkedList.insertInBetween(firstNode,newNode);
+        System.out.println(                             );
+        System.out.println("After");
         linkedList.print();
     }
 }
