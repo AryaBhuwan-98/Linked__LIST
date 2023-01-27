f4768600f9ef683073b28d56df00a56c136438d4
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-26 19:52:59 -0800

    Add:Welcome to the Data Structure Linked List Program

---
+++ src/main/java/com/datastructure/App.java
@@ -0,0 +1,9 @@
+package com.datastructure;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Data Structure Linked List" );
+    }
+}
