82ed16d8a7253b377db2eaad8a1f7d97e2872f61
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-26 21:35:29 -0800

    Added: Ability to Delete From The Linked List Sequence list

--- src/main/java/com/datastructure/Main.java
+++ src/main/java/com/datastructure/Main.java
@@ -9,40 +9,44 @@ import java.util.Scanner;
 
 public class Main {
             
-            public static void main(String[] args) {
-                System.out.println("Welcome to linkedList DataStructure Program");
-                System.out.println("Welcome to linkedList DataStructure Program");
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Enter 1 to add the data at the start"); // UC 2
-                System.out.println("Enter 2 to add the data at the End"); // UC 3
-                System.out.println("Enter 3 to add the data at In Between "); // UC 4
-                System.out.println("Enter 4 to delete the data at first position "); // UC 5
-                System.out.println("Enter 5 to delete the data at last position "); // UC 6
-                System.out.println("Enter 6 to search the data at  position "); // UC 7
-                System.out.println("Enter 7 to insert  the data at index position "); // UC 8
-                switch (scanner.nextInt()) {
-        
-                    case 1:
-                        Operation.addDataAtStart();
-                        break;
-                    case 2:
-                        Operation.addDataAtEnd();
-                        break;
-                    case 3:
-                        Operation.insertInBetween();
-                        break;
-                    case 4:
-                        Operation.pop();
-                        break;
-                    case 5:
-                        Operation.popLast();
-                        break;
-                    case 6:
-                        Operation.search();
-                        break;
-                    case 7:
-                        Operation.insertAtSpecificIndex();
-                        break;
-                }
-            }
+    public static void main(String[] args) {
+        System.out.println("Welcome to linkedList DataStructure Program");
+        System.out.println("Welcome to linkedList DataStructure Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start"); // UC 2
+        System.out.println("Enter 2 to add the data at the End"); // UC 3
+        System.out.println("Enter 3 to add the data at In Between "); // UC 4
+        System.out.println("Enter 4 to delete the data at first position "); // UC 5
+        System.out.println("Enter 5 to delete the data at last position "); // UC 6
+        System.out.println("Enter 6 to search the data at  position "); // UC 7
+        System.out.println("Enter 7 to insert  the data at index position "); // UC 8
+        System.out.println("Enter 8 to delete 40 from list "); // UC 9
+        switch (scanner.nextInt()) {
+
+            case 1:
+                Operation.addDataAtStart();
+                break;
+            case 2:
+                Operation.addDataAtEnd();
+                break;
+            case 3:
+                Operation.insertInBetween();
+                break;
+            case 4:
+                Operation.pop();
+                break;
+            case 5:
+                Operation.popLast();
+                break;
+            case 6:
+                Operation.search();
+                break;
+            case 7:
+                Operation.insertAtSpecificIndex();
+                break;
+            case 8:
+                Operation.deleteAtIndexPosition();
+                break;
         }
+    }
+}
--- src/main/java/com/datastructure/Operation.java
+++ src/main/java/com/datastructure/Operation.java
@@ -2,86 +2,119 @@ package com.datastructure;
 
 public class Operation {
     
-   //created method to add data at start.
-   public static void addDataAtStart(){
-    LinkedList linkedList =new LinkedList();
-    linkedList.push(70);
-    linkedList.push(30);
-    linkedList.push(56);
-   linkedList.print();
-}
+    public static void addDataAtStart(){
+        LinkedList linkedList =new LinkedList();
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+       linkedList.print();
+        System.out.println(" ");
+        linkedList.size();
+    }
 
 // Created a method to add data at end.
-public static void addDataAtEnd() {
-    LinkedList linkedList = new LinkedList();
-    linkedList.append(56);
-    linkedList.append(30);
-    linkedList.append(70);
-    linkedList.print();
+    public static void addDataAtEnd() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(56);
+        linkedList.append(30);
+        linkedList.append(70);
+        linkedList.print();
 
-}
-//created method for insert in between
-public static void insertInBetween(){
-    LinkedList linkedList=new LinkedList();
-    Node secondNode =linkedList.push(70);
-    Node firstNode =linkedList.push(56);
-    Node newNode =new Node(30);
-    System.out.println("Before");
-    linkedList.print();
-    linkedList.insertInBetween(firstNode,newNode);
-    System.out.println(                             );
-    System.out.println("After");
-    linkedList.print();
-}
-// create a method for delete first  element
-public static void pop(){
-    LinkedList linkedList=new LinkedList();
-   Node firstNode= linkedList.push(70);
-    Node secondNode= linkedList.push(30);
-    Node newNode=linkedList.push(56);
-    System.out.println("Before");
-    linkedList.print();
-   linkedList.pop();
-    System.out.println(            );
-    System.out.println("After");
-    linkedList.print();
+    }
+    //created method for insert in between
+    public static void insertInBetween(){
+        LinkedList linkedList=new LinkedList();
+        Node secondNode =linkedList.push(70);
+        Node firstNode =linkedList.push(56);
+        Node newNode =new Node(30);
+        System.out.println("Before");
+        linkedList.print();
+        linkedList.insertInBetween(firstNode,newNode);
+        System.out.println(                             );
+        System.out.println("After");
+        linkedList.print();
+        System.out.println(" ");
+        linkedList.size();
+    }
+    // create a method for delete first  element
+    public static void pop(){
+        LinkedList linkedList=new LinkedList();
+       Node firstNode= linkedList.push(70);
+        Node secondNode= linkedList.push(30);
+        Node newNode=linkedList.push(56);
+        System.out.println("Before");
+        linkedList.print();
+       linkedList.pop();
+        System.out.println(            );
+        System.out.println("After");
+        linkedList.print();
+        System.out.println(" ");
+        linkedList.size();
 
 
 }
 //method to delete last element
-public static void popLast(){
+    public static void popLast(){
+        LinkedList linkedList=new LinkedList();
+        Node firstNode= linkedList.push(70);
+        Node secondNode= linkedList.push(30);
+        Node newNode=linkedList.push(56);
+        System.out.println("Before");
+        linkedList.print();
+        System.out.println(         );
+        linkedList.popLast();
+        System.out.println("After");
+        linkedList.print();
+        System.out.println(" ");
+        linkedList.size();
+
+}
+//method to search specific value
+public static void search(){
     LinkedList linkedList=new LinkedList();
     Node firstNode= linkedList.push(70);
     Node secondNode= linkedList.push(30);
     Node newNode=linkedList.push(56);
     System.out.println("Before");
     linkedList.print();
-    System.out.println(         );
-    linkedList.popLast();
+    linkedList.search(30);
+    System.out.println(" ");
+    linkedList.size();
+    }
+public static void insertAtSpecificIndex() {
+    LinkedList linkedList = new LinkedList();
+    Node firstNode = linkedList.push(70);
+    Node secondNode = linkedList.push(30);
+    Node newNode = linkedList.push(56);
+    System.out.println("Before");
+    linkedList.print();
+    System.out.println("                                      ");
+    linkedList.insertAtSpecificIndex(2, 40);
     System.out.println("After");
     linkedList.print();
+    System.out.println(" ");
+    linkedList.size();
 
 }
-//method to search specific value
-public static void search(){
-LinkedList linkedList=new LinkedList();
-Node firstNode= linkedList.push(70);
-Node secondNode= linkedList.push(30);
-Node newNode=linkedList.push(56);
-System.out.println("Before");
-linkedList.print();
-linkedList.search(30);
-}
-public static void insertAtSpecificIndex() {
-LinkedList linkedList = new LinkedList();
-Node firstNode = linkedList.push(70);
-Node secondNode = linkedList.push(30);
-Node newNode = linkedList.push(56);
-System.out.println("Before");
-linkedList.print();
-System.out.println("                                      ");
-linkedList.insertAtSpecificIndex(2, 40);
-System.out.println("After");
-linkedList.print();
+public static void deleteAtIndexPosition(){
+    LinkedList linkedList = new LinkedList();
+    Node firstNode = linkedList.push(70);
+    Node secondNode = linkedList.push(30);
+    Node newNode = linkedList.push(56);
+    System.out.println("Before");
+    linkedList.print();
+    System.out.println("                                      ");
+    linkedList.insertAtSpecificIndex(2, 40);
+    System.out.println("After");
+    linkedList.print();
+    System.out.println(" ");
+    linkedList.size();
+    System.out.println("      ");
+    System.out.println("After Deleting The Value");
+    linkedList.deleteAtIndexPosition(40);
+    linkedList.print();
+    System.out.println(" ");
+    linkedList.size();
+
 }
 }
